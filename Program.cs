596888964cd2ad3617fe2b162d4e3d01349f60be
Josan Johnata <josanjohnata@gmail.com>
2021-08-28 20:36:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 5;
+      for (int i = 0; i < number; i++)
+      {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+      }
+    }
+  }
+}
