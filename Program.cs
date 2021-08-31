33e68f7242110f95c26679c10d2def9897f60d5d
Josan Johnata <josanjohnata@gmail.com>
2021-08-31 10:23:20 -0300

    Exemplificamdo o conteudo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Instrucoes
+{
+  class Program
+  {
+    static void Declaracoes()
+    {
+      init a;
+      init b = 2, c = 3;
+      const init d = 4;
+      a = 1;
+      Console.WriteLine(a + b + c + d);
+    }
+
+    static void InstrucoesIf(string[] args)
+    {
+      if (args.Length == 0) {
+        Console.WriteLine("Nenhum argumento");
+      }
+      else if (args.Length == 1) {
+        Console.WriteLine("Um argumento");
+      }
+      else {
+        Console.WriteLine("${args.Length} argumentos");
+      }
+    }
+  }
+}
