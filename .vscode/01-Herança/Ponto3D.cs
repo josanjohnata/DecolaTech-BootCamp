b1a8a25a553c24af36869c17a3bce25b197dde42
Josan Johnata <josanjohnata@gmail.com>
2021-08-31 13:10:04 -0300

    Classes publicas e privadas

---
+++ .vscode/01-Herança/Ponto3D.cs
@@ -0,0 +1,21 @@
+namespace DigitalInnovationOne.Herança
+{
+  public class Ponto3D : Ponto
+  {
+    public int z;
+    public Ponto3D(int x, int y, int z) : base(x, y)
+    {
+      this.z = z;
+      CalcularDistancia();
+    }
+
+    public static void Calcular()
+    {
+
+    }
+
+    public override void CalcularDistancia3(){
+      
+    }
+  }
+}
